7720ab8ff49f56a375439432dfbf7c4f9003d9ef
RightWing <alex.iskorkin@gmail.com>
2021-06-17 17:04:55 +0300

    added registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcryptjs = require('bcryptjs')
+const User = require('../models/User')
+
 module.exports.login = function(req, res) {
   res.status(200).json({
     login: {
@@ -8,8 +11,32 @@ module.exports.login = function(req, res) {
 }
 
 
-module.exports.register = function(req, res) {
-  res.status(200).json({
-    register: 'from controller'
-  })
+module.exports.register = async function(req, res) {
+  const checkUser = await  User.findOne({email: req.body.email})
+
+  if (checkUser) {
+    res.status(409).json({
+      message: 'User is registered, sorry man'
+    })
+  }
+  else {
+    const salt = bcryptjs.genSaltSync(12)
+    const user = new User ({
+      email: req.body.email,
+      password: bcryptjs.hashSync(req.body.password, salt)
+    })
+    try{
+      await user.save()
+      res.status(201).json({
+        message: 'User is saved',
+        user: user
+      })
+    }catch(e){
+      res.status(408).json({
+        message: 'User is not saved',
+        exception: e
+      })
+    }
+  }
+
 }
